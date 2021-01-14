647c6519174f1b3022c2efc24436d83178140f0d
RocketRoss <code@radonn.co.za>
2021-01-14 10:47:10 +0000

    ^~ 1st pkt_seq=obsstart, +robust, packets nonseq

--- src/hpguppi_atasnap_pktsock_thread.c
+++ src/hpguppi_atasnap_pktsock_thread.c
@@ -839,7 +839,7 @@ static void *run(hashpipe_thread_args_t * args)
             if (state != RECORD && ata_snap_obs_info_valid(obs_info)){// Only enter recording mode if obs_params are valid
               flag_state_update = 1;
               // flag_obs_start = flag_state_update;
-              first_pkt_seq_num = pkt_seq_num;
+              first_pkt_seq_num = obs_start_pktidx;
               ata_snap_obs_info_read(st, &obs_info);
               obs_npacket_total = 0;
               obs_ndrop_total = 0;
