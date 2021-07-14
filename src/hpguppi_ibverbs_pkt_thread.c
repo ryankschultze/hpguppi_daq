579ed11cc30b09e850ca5d09771d4bd7fcd28dfa
RocketRoss <code@radonn.co.za>
2021-07-14 10:04:44 +0000

    vv ibverbs_pkt thread revert to multiple recv_mrs 1af5e0022233472562ac52ec2af6d4886180c5bc

--- src/hpguppi_ibverbs_pkt_thread.c
+++ src/hpguppi_ibverbs_pkt_thread.c
@@ -451,7 +451,7 @@ hpguppi_ibverbs_init(struct hashpipe_ibv_context * hibv_ctx,
   // Send memory region is just one packet.  Recv memory region spans a data block, with
   // one recv memory region registered per block (see recv_mr_num).
   hibv_ctx->send_mr_size = (size_t)hibv_ctx->send_pkt_num * hibv_ctx->pkt_size_max;
-  hibv_ctx->recv_mr_size = sizeof(db->block);
+  hibv_ctx->recv_mr_size = sizeof(db->block[0].data);
 
   // Allocate memory for send_mr_buf
   if(!(hibv_ctx->send_mr_buf = (uint8_t *)calloc(
@@ -459,7 +459,11 @@ hpguppi_ibverbs_init(struct hashpipe_ibv_context * hibv_ctx,
     return HASHPIPE_ERR_SYS;
   }
   // Point recv_mr_buf to starts of block 0
-  hibv_ctx->recv_mr_buf = (uint8_t *)db->block;
+  hibv_ctx->recv_mr_num = db->header.n_block;
+  hibv_ctx->recv_mr_bufs = malloc(hibv_ctx->recv_mr_num * sizeof(uint8_t *));
+  for(i=0; i<hibv_ctx->recv_mr_num; i++){
+    hibv_ctx->recv_mr_bufs[i] = (uint8_t *)db->block[i].data;
+  }
 
   // Setup send WR's num_sge and SGEs' addr/length fields
   hibv_ctx->send_pkt_buf[0].wr.num_sge = 1;
@@ -813,6 +817,7 @@ int debug_i=0, debug_j=0;
       base_addr = (uint64_t)hpguppi_pktbuf_block_slot_ptr(db, next_block, next_slot);
       for(i=0; i<num_chunks; i++) {
         curr_rpkt->wr.sg_list[i].addr = base_addr + chunks[i].chunk_offset;
+        curr_rpkt->wr.sg_list[i].lkey = hibv_ctx->recv_mrs[next_block % db->header.n_block]->lkey;
       }
 
       // Advance slot
